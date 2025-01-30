1d70202d70eb5403e97dece48f3fd38999b730cc
andreo210 <98775473+andreo210@users.noreply.github.com>
2025-01-30 08:24:05 -0300

    rótulos e titulos

---
+++ Matplotlib/rotulos e titulos.py
@@ -0,0 +1,71 @@
+"""
+Criar rótulos para um gráfico
+Com o Pyplot, você pode usar as funções xlabel() e ylabel() para definir um rótulo para os eixos x e y.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.show()
+
+
+
+
+"""
+Crie um título para um enredo
+Com o Pyplot, você pode usar a title() função para definir um título para o gráfico.
+"""
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.show()
+
+
+
+
+"""
+Definir propriedades de fonte para título e rótulos
+Você pode usar o fontdictparâmetro em xlabel(), ylabel(), e title() para definir propriedades de fonte para o título e rótulos.
+"""
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+font1 = {'family':'serif','color':'blue','size':20}
+font2 = {'family':'serif','color':'darkred','size':15}
+
+plt.title("Sports Watch Data", fontdict = font1)
+plt.xlabel("Average Pulse", fontdict = font2)
+plt.ylabel("Calorie Burnage", fontdict = font2)
+
+plt.plot(x, y)
+plt.show()
+
+
+
+""""
+Posicione o Título
+Você pode usar o locparâmetro in title()para posicionar o título.
+Os valores legais são: 'left', 'right' e 'center'. O valor padrão é 'center'.
+"""
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data", loc = 'left')
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+plt.show()
