4af5913b092d973c2b350a9f39abbb8a0f7d09b0
andreo210 <98775473+andreo210@users.noreply.github.com>
2025-03-07 08:52:25 -0300

    Numpay

---
+++ NumPy/basico/inicio.py
@@ -0,0 +1,16 @@
+"""
+Aprenda NumPy
+NumPy é uma biblioteca Python.
+O NumPy é usado para trabalhar com matrizes.
+NumPy é a abreviação de "Numerical Python".
+"""
+
+
+"""
+Exemplo
+Crie uma matriz NumPy:
+"""
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+print(type(arr))
